441fc49c2c549742aed8487c9e127e830a5b03f7
Anderson Novais <pimentelwork.info@gmail.com>
2026-08-27 11:12:26 -0300

    Criado menu interativo utilizando o readline e require ( input e output ).
    Proximas alterações será trabalhar com module e criar os proximos menus ( criação de personagem, seleção do pokemon inicial ).

--- index.js
+++ index.js
@@ -1,5 +1,37 @@
+const readline = require('readline');
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout 
+});
+
+function menuInicial() { 
+
 console.log('==Bem vindo ao pokemonJS==')
-console.log('= New Game =')
-console.log('= Continue =')
-console.log('=  Opções  =')
-console.log('=   Sair   =')
+console.log('= 1. New Game =')
+console.log('= 2. Continue =')
+console.log('=  3. Opções  =')
+console.log('=   0. Sair   =')
+
+    rl.question('Escolha uma opção: ', (opcao) => {
+        switch(opcao){
+            case '1':
+                console.log('\nIniciando novo jogo...');
+                return;
+            case '2':
+                console.log('\nContinuar jogo...');
+                break;
+            case '3':
+                console.log('\nAbrindo opções...');
+                break;
+            case '0':
+                console.log('\nEncerrando jogo...');
+                break;
+        }
+
+        rl.close();
+    })
+
+}
+
+menuInicial();
